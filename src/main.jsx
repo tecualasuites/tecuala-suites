9e89b924440aea3f8f54679eef1cbc118a7ca8e4
Tecuala Suites <tecualasuites@icloud.com>
2026-06-05 10:12:20 -0700

    Remove email from header

--- src/main.jsx
+++ src/main.jsx
@@ -645,7 +645,6 @@ function App() {
           <img className="brand-logo" src="/images/tecuala-logo.png" alt="Tecuala Suites" />
           <div>
             <h1>{tr("businessName")}</h1>
-            <a href={`mailto:${BUSINESS_EMAIL}`}>{BUSINESS_EMAIL}</a>
           </div>
         </div>
         <div className="header-actions">
